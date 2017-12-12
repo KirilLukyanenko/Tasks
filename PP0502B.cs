50edb27fd2c6ce175bfab12f18d5312c37bef948
klukyanenko <klukyanenko1993@gmail.com>
2017-12-12 02:05:44 +0200

    Solution of the task "PP0502B"

---
+++ PP0502B.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var count = Convert.ToInt32(Console.ReadLine());
+            var rezults = new List<List<string>>();
+            for (int i = 0; i < count; i++)
+            {
+                string input = Console.ReadLine();
+                var arrayInput = input.Split(new char[] { ' ' }).ToList();
+
+                arrayInput.RemoveAt(0);
+                arrayInput.Reverse();
+                rezults.Add(arrayInput.ToList());
+            }
+
+            foreach (var value in rezults)
+            {
+                var str = String.Empty;
+
+                foreach (var item in value)
+                    str += String.Format("{0} ", item);
+
+                Console.WriteLine(str);
+            }
+        }
+    }
+}
